8ad9ab4182e67d6b99b1575627ae1f49f4de5f1d
lelbarton <lauraelbarton@gmail.com>
2015-05-29 10:36:06 -0700

    Implement first shot at main method

--- src/ca/ubc/ca/ocrreader/ContextCompareLoader.java
+++ src/ca/ubc/ca/ocrreader/ContextCompareLoader.java
@@ -1,10 +1,26 @@
 package ca.ubc.ca.ocrreader;
 
+import java.io.File;
+
 public class ContextCompareLoader {
 
+
 	public static void main(String[] args) {
-		// get the file where images are stored
+		ContextManager manager = new ContextManager();
+		// the folder where images are stored should be passed as the param
+		// FIXME currently hard coded for testing purposes
+		// TODO add last modified time to files, ContextManager and Context
+		// constructor
+
+		// get the directory where images are stored
+		File imgDir = new File("~/Desktop/testImageFiles");
+
 		// iterate over all files and pass to ContextManager
-		// delete files when done?
+		File[] files = imgDir.listFiles();
+
+		for (File file : files) {
+			// TODO check if this is an image file, not other or dir
+			manager.addContext(file);
+		}
 	}
 }
