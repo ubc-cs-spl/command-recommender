3a7cb75d8951d12ea5c3467f3c09fa3d2fe52423
lelbarton <lauraelbarton@gmail.com>
2015-05-29 10:15:56 -0700

    Add class ContextCompareLoader for main method

---
+++ src/ca/ubc/ca/ocrreader/ContextCompareLoader.java
@@ -0,0 +1,10 @@
+package ca.ubc.ca.ocrreader;
+
+public class ContextCompareLoader {
+
+	public static void main(String[] args) {
+		// get the file where images are stored
+		// iterate over all files and pass to ContextManager
+		// delete files when done?
+	}
+}
